43ef173460a719f81b5f583f2b0417aa6e90b776
team-irs5993 <91738310+team-irs5993@users.noreply.github.com>
2024-02-10 19:54:49 +0300

    DrivetrainSubsystem construction

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  /** Creates a new DrivetrainSubsystem. */
+
+  private final PWMVictorSPX leftMotor;
+  private final PWMVictorSPX rightMotor;
+
+  private final DifferentialDrive driveBase;
+  private final AHRS gyro;
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Yaw", getYaw());
+    SmartDashboard.putNumber("Pitch", getPitch());
+  }
+
+  public DrivetrainSubsystem() {
+
+    leftMotor = new PWMVictorSPX(0);
+    rightMotor = new PWMVictorSPX(1);
+    rightMotor.setInverted(true);
+
+    driveBase = new DifferentialDrive(leftMotor, rightMotor);
+    gyro = new AHRS(SPI.Port.kMXP);
+  }
+
+  public void drive(double xSpeed, double zRotation) {
+    driveBase.arcadeDrive(xSpeed, zRotation);
+  }
+
+  public void resetGyro() {
+    gyro.reset();
+  }
+
+  public double getYaw() {
+    return gyro.getYaw();
+  }
+
+  public double getPitch() {
+    return gyro.getPitch();
+  }
+
+  public void stop() {
+    driveBase.stopMotor();
+  }
+}
