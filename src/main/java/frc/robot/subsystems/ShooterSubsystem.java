d90c9b0223fcf046b628a8745bde3bcecabe80c2
Kağan Atalay <48818679+kaganatalay@users.noreply.github.com>
2024-02-14 01:30:24 +0300

    Added ShooterSubsystem

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CANIDS;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class ShooterSubsystem extends SubsystemBase {
+
+  private final CANSparkMax leftMotor;
+  private final CANSparkMax rightMotor;
+
+  public ShooterSubsystem() {
+    leftMotor = new CANSparkMax(CANIDS.SHOOTER_LEFT, MotorType.kBrushless);
+    rightMotor = new CANSparkMax(CANIDS.SHOOTER_RIGHT, MotorType.kBrushless);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setLeftMotorSpeed(double speed) {
+    leftMotor.set(speed);
+  }
+
+  public void setRightMotorSpeed(double speed) {
+    rightMotor.set(speed);
+  }
+
+  public void stop() {
+    leftMotor.stopMotor();
+    rightMotor.stopMotor();
+  }
+}
