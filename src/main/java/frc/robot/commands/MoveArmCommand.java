b7547b3e5b62311242ee2538f3f8957fb9f89194
team-irs5993 <91738310+team-irs5993@users.noreply.github.com>
2024-02-14 16:36:34 +0300

    Added MoveArmCommand

---
+++ src/main/java/frc/robot/commands/MoveArmCommand.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class MoveArmCommand extends Command {
+  private final ArmSubsystem armSubsystem;
+  private double speed;
+
+  public MoveArmCommand(ArmSubsystem armSubsystem, double speed) {
+    addRequirements(armSubsystem);
+
+    this.armSubsystem = armSubsystem;
+    this.speed = speed;
+  }
+
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    this.armSubsystem.setMotorSpeed(speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    this.armSubsystem.stop();
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
